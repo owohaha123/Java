123d25ef3ae236e89991ce9f40fbe778b86718e6
김윤주 <owohaha123@gmail.com>
2022-05-04 20:57:48 +0900

    220504
    강제 별찍기 당함

---
+++ SweetPotato/src/loop/Star.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class Star {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 0; j < i; j++) { 
+				System.out.print("*");
+			}
+			System.out.println("");//enter
+
+		}
+	}
+
+}
